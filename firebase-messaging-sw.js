56d2be53a5f531d1da172bfd32ab72005c9c4340
WallFlixHub <wallflix2@gmail.com>
2026-06-09 18:57:58 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,14 +2,15 @@ importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDLv0FWvThD7h6EnHMb-l9Tn0pfOLfbvGE",
-  authDomain: "wallflixhub.firebaseapp.com",
-  projectId: "wallflixhub",
-  storageBucket: "wallflixhub.firebasestorage.app",
-  messagingSenderId: "621224536171",
-  appId: "1:621224536171:web:f48f9a44609741fb63692e",
-  measurementId: "G-BYR8JH0QTM"
-});
+  apiKey: "AIzaSyAzP0RUkpglCJGLSG3U_f_MDnA4Qkh1r0g",
+  authDomain: "wallflixhub-io.firebaseapp.com",
+  databaseURL: "https://wallflixhub-io-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "wallflixhub-io",
+  storageBucket: "wallflixhub-io.firebasestorage.app",
+  messagingSenderId: "331742171946",
+  appId: "1:331742171946:web:3fecda85d1597baa970453",
+  measurementId: "G-E29GMX88E5"
+};
 
 const messaging = firebase.messaging();
 
